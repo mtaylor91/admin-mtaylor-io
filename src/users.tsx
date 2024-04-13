7c50d263c0fc4a1e7d6fbdacecbba033e3181f7b
Mike Taylor <mike.charles.taylor@gmail.com>
2024-04-13 12:53:31 -0600

    create user form

--- src/users.tsx
+++ src/users.tsx
@@ -1,3 +1,4 @@
+import { route } from 'preact-router'
 import { useState, useEffect } from 'preact/hooks'
 import IAM, { UserIdentity } from 'iam-mtaylor-io-js'
 import { resolveUserIdentifier } from './util'
@@ -11,6 +12,8 @@ interface UsersViewProps {
 
 export function UsersView({ client }: UsersViewProps) {
   const [users, setUsers] = useState<UserIdentity[]>([])
+  const [showCreateUserForm, setShowCreateUserForm] = useState(false)
+  const [createUserEmail, setCreateUserEmail] = useState('')
 
   useEffect(() => {
     const getUsers = async () => {
@@ -21,15 +24,60 @@ export function UsersView({ client }: UsersViewProps) {
     getUsers()
   }, [])
 
-  return (
-    <div>
-      <h1>Users</h1>
-      <ul>
-        {users.map(user => {
-          const userId = resolveUserIdentifier(user)
-          return (<li><a href={`/users/${userId}`}>{userId}</a></li>)
-        })}
-      </ul>
-    </div>
-  )
+  const onClickCreateUser = async (event: Event) => {
+    event.preventDefault()
+    setShowCreateUserForm(true)
+  }
+
+  const onSubmitCreateUser = async (event: Event) => {
+    event.preventDefault()
+    if (createUserEmail === '') {
+      const principal = await client.users.createUser()
+      route(`/users/${resolveUserIdentifier(principal.user)}`)
+    } else {
+      const principal = await client.users.createUser(createUserEmail)
+      route(`/users/${resolveUserIdentifier(principal.user)}`)
+    }
+    setShowCreateUserForm(false)
+  }
+
+  const onCancelCreateUser = async (event: Event) => {
+    event.preventDefault()
+    setShowCreateUserForm(false)
+  }
+
+  const onInputCreateUserEmail = async (event: Event) => {
+    const target = event.target as HTMLInputElement
+    setCreateUserEmail(target.value)
+  }
+
+  if (showCreateUserForm) {
+    return (
+      <div>
+        <h1>Create User</h1>
+        <form onSubmit={onSubmitCreateUser}>
+          <label>
+            Email
+            <input type="email" value={createUserEmail}
+              onInput={onInputCreateUserEmail} />
+          </label>
+          <button type="submit">Create</button>
+          <button onClick={onCancelCreateUser}>Cancel</button>
+        </form>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>Users</h1>
+        <button onClick={onClickCreateUser}>Create User</button>
+        <ul>
+          {users.map(user => {
+            const userId = resolveUserIdentifier(user)
+            return (<li><a href={`/users/${userId}`}>{userId}</a></li>)
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
