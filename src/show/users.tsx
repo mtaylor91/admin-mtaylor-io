00802ae0319c4e156091682ea13ef4e4918be570
Mike Taylor <mike.charles.taylor@gmail.com>
2024-04-23 06:49:13 -0600

    Add user pagination

--- src/show/users.tsx
+++ src/show/users.tsx
@@ -5,6 +5,37 @@ import IAM, { UserIdentity } from 'iam-mtaylor-io-js'
 import { resolveUserId, resolveUserIdentifier } from '../util'
 
 
+interface PaginationProps {
+  offset: number,
+  limit: number,
+  total: number
+}
+
+
+function Pagination({ offset, limit, total }: PaginationProps) {
+  const page = Math.floor(offset / limit) + 1
+  const pages = Math.ceil(total / limit)
+  const prevOffset = offset - limit
+  const nextOffset = offset + limit
+
+  return (
+    <div class="pagination">
+      <span>
+        {offset > 0 &&
+          <Link href={`/users?offset=${prevOffset}&limit=${limit}`}>Previous</Link>
+        }
+      </span>
+      <span>Page {page} of {pages}</span>
+      <span>
+        {page < pages &&
+        <Link href={`/users?offset=${nextOffset}&limit=${limit}`}>Next</Link>
+        }
+      </span>
+    </div>
+  )
+}
+
+
 interface UsersTableProps {
   users: UserIdentity[]
 }
@@ -46,19 +77,27 @@ function UsersTable({ users }: UsersTableProps) {
 
 interface UsersViewProps {
   client: IAM,
-  path?: string
+  path?: string,
+  offset?: number,
+  limit?: number
 }
 
 
-export function ShowUsers({ client }: UsersViewProps) {
+export function ShowUsers(props: UsersViewProps) {
+  const { client } = props
+  const [total, setTotal] = useState(0)
   const [users, setUsers] = useState<UserIdentity[]>([])
   const [error, setError] = useState<string | null>(null)
+
+  const offset = Number(props.offset) || 0
+  const limit = Number(props.limit) || 100
 
   useEffect(() => {
     const getUsers = async () => {
       try {
-        const response = await client.users.listUsers()
+        const response = await client.users.listUsers(offset, limit)
         const users = response.items
+        setTotal(response.total)
         setError(null)
         setUsers(users)
       } catch (err) {
@@ -71,7 +110,7 @@ export function ShowUsers({ client }: UsersViewProps) {
     }
 
     getUsers()
-  }, [])
+  }, [offset, limit])
 
   return (
     <div class="section">
@@ -79,6 +118,7 @@ export function ShowUsers({ client }: UsersViewProps) {
         <Link href="/create/user">Create User</Link>
       </div>
       <UsersTable users={users} />
+      <Pagination offset={offset} limit={limit} total={total} />
       <div class="menubar">
         {error && <p class="error">{error}</p>}
       </div>
